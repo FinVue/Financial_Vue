d9644e26b2d42687ab25ee53d02f54a29e949833
Rhyu Miranda <rhyu.agapito.miranda@gmail.com>
2024-05-17 23:51:11 +0800

    refactor: add fonts from design system

--- template-ui/tailwind.config.js
+++ template-ui/tailwind.config.js
@@ -3,6 +3,17 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {
+      fontSize: {
+        'heading-1': ['64px'],
+        'heading-2': ['40px'],
+        'heading-3': ['24px'],
+        'subtitle': ['24px'],
+        'body': ['16px'],
+        'small': ['14px'],
+        'pre-title': ['10px'],
+        'btn-text': ['10px'],
+        'link': ['16px'],
+      },
       fontFamily: {
         montserrat: ['Montserrat', 'sans-serfif'],
       },
