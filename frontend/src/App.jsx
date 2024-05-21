79b1adf7f5f32edb6ce382c1704f170f9c9fe1f9
Rhyu Miranda <rhyu.agapito.miranda@gmail.com>
2024-05-21 19:41:15 +0800

    feat: create routes for respective pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,31 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Login from "./pages/guests/Login"
+import Register from "./pages/guests/Register"
+import Dashboard from "./pages/user/Dashboard"
+import Income from "./pages/user/Income"
+import Expense from "./pages/user/Expense"
+import EmergencyFund from "./pages/user/EmergencyFund"
+import TaxCalculator from "./pages/user/TaxCalculator"
+import Settings from "./pages/user/Settings"
+import Layout from "./pages/Layout"
+
 function App() {
   
   return (
-    <>
-      Hello!
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Layout/>}>
+          <Route path="/login" element={<Login/>}/>
+          <Route path="/register" element={<Register/>}/>
+          <Route path="/dashboard" element={<Dashboard/>}/>
+          <Route path="/income" element={<Income/>}/>
+          <Route path="/expense" element={<Expense/>}/>
+          <Route path="/emergency-fund" element={<EmergencyFund/>}/>
+          <Route path="/tax-calculator" element={<TaxCalculator/>}/>
+          <Route path="/settings" element={<Settings/>}/>
+        </Route>
+      </Routes>
+    </BrowserRouter>  
   )
 }
 
