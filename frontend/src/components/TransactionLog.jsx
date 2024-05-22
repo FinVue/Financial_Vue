8fcc6bfef74be40c05e57442d9873b6a27795b3d
Rhyu Miranda <rhyu.agapito.miranda@gmail.com>
2024-05-23 02:20:59 +0800

    fix: create a parent component for transaction category

--- frontend/src/components/TransactionLog.jsx
+++ frontend/src/components/TransactionLog.jsx
@@ -2,16 +2,14 @@ import TransactionCategory from "./TransactionCategory";
 
 TransactionCategory;
 
-function TransactionLog() {
+function TransactionLog({children}) {
   return (
     <article className="px-6 py-4">
       <p className="text-body text-white py-4">
         Last 10 Transactions &gt;{" "}
         <span className="text-body underline font-bold">View All</span>
       </p>
-      <div className="flex flex-col gap-2">
-        <TransactionCategory />
-      </div>
+      {children}
     </article>
   );
 }
